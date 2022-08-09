e25f9442c6e382ca47b3816ac6ef1d4c28571b43
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-09 10:30:01 -0600

    Redux installed

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import configureStore from "../configureStore"
 import HelloWorld from "./HelloWorld"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+const store = configureStore();
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element= {<h1>Home</h1>} />
-          <Route path="/hello" element= {<HelloWorld greeting="Friend" />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element= {<h1>Home</h1>} />
+            <Route path="/hello" element= {<HelloWorld greeting="Friend" />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
